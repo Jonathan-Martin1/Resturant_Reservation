dfa02c7bf29caf576926f02ab0826c13feb90ca7
Jonathan <oldschoolspaceempire@gmail.com>
2021-04-21 12:37:59 -0500

    added API connection

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  'https://fc-back-end-2zw19zw5j-jonathanmartin-gif.vercel.app/'
+  'https://fc-back-end-qora8hz5x-jonathanmartin-gif.vercel.app/'
 
 /**
  * Defines the default headers for these functions to work with `json-server`
